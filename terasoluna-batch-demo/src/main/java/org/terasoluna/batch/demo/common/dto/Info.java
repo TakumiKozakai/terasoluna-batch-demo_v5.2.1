b75fa4e2288dc34ce235d8032556c7e153f9a70b
T.K <2b9sa2emil@gmail.com>
2020-05-31 18:24:44 +0900

    lombok導入

--- terasoluna-batch-demo/src/main/java/org/terasoluna/batch/demo/common/dto/Info.java
+++ terasoluna-batch-demo/src/main/java/org/terasoluna/batch/demo/common/dto/Info.java
@@ -1,33 +1,12 @@
 package org.terasoluna.batch.demo.common.dto;
 
+import lombok.Data;
+
+@Data
 public abstract class Info {
 
 	private int updateKbn;
 	private String createDate;
 	private String updateDate;
-
-	public int getUpdateKbn() {
-		return updateKbn;
-	}
-
-	public void setUpdateKbn(int updateKbn) {
-		this.updateKbn = updateKbn;
-	}
-
-	public String getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-
-	public String getUpdateDate() {
-		return updateDate;
-	}
-
-	public void setUpdateDate(String updateDate) {
-		this.updateDate = updateDate;
-	}
 
 }
