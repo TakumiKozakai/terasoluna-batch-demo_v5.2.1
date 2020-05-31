bac717e98e4214edee62ef08f9428a5a18320e6d
T.K <2b9sa2emil@gmail.com>
2020-05-31 18:25:18 +0900

    処理時間計測処理追加

--- terasoluna-batch-demo/src/main/java/org/terasoluna/batch/demo/common/listener/JobExecutionLoggingListener.java
+++ terasoluna-batch-demo/src/main/java/org/terasoluna/batch/demo/common/listener/JobExecutionLoggingListener.java
@@ -10,16 +10,28 @@ import org.springframework.stereotype.Component;
 public class JobExecutionLoggingListener implements JobExecutionListener {
 
 	private static final Logger logger = LoggerFactory.getLogger(JobExecutionLoggingListener.class);
+	private static long startMillisTime = 0;
+	private static long endMillisTime = 0;
 
 	@Override
 	public void beforeJob(JobExecution jobExecution) {
 		logger.info("job started. [JobName:{}]", jobExecution.getJobInstance().getJobName());
+
+		// 開始時間計測
+		startMillisTime = System.currentTimeMillis();
+		logger.trace("job TRACE started. [JobName:{}]", jobExecution.getJobInstance().getJobName());
 	}
 
 	@Override
 	public void afterJob(JobExecution jobExecution) {
-
 		logger.info("job finished.[JobName:{}][ExitStatus:{}]", jobExecution.getJobInstance().getJobName(),
 				jobExecution.getExitStatus().getExitCode());
+
+		// 終了時間計測
+		endMillisTime = System.currentTimeMillis();
+		// 処理時間算出
+		long milli = endMillisTime - startMillisTime;
+		String strMilli = String.valueOf(milli);
+		logger.trace("job TRACE finished. [JobName:{}] : 処理時間（ミリ）： " + strMilli + " ms", jobExecution.getJobInstance().getJobName());
 	}
 }
