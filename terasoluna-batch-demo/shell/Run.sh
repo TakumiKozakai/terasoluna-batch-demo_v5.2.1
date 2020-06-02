87b8792db57224a63c26d0bf204fe9a50966c718
T.K <2b9sa2emil@gmail.com>
2020-06-02 17:32:47 +0900

    処理をInfo1のみに変更

--- terasoluna-batch-demo/shell/Run.sh
+++ terasoluna-batch-demo/shell/Run.sh
@@ -38,40 +38,5 @@ echo '処理結果:' $?
 
 wait $!
 
-echo '＝＝＝Info2＝＝＝'
-#CSV→DBタスクレット
-echo '＝＝＝入力処理開始＝＝＝'
-start_time=`date +%s`
-
-java -cp 'lib/*:target/*' \
-org.springframework.batch.core.launch.support.CommandLineJobRunner \
-META-INF/jobs/csvtodb/Info2Tasklet.xml \
-Info2Tasklet \
-inputFile=files/input/input-info2-data.csv
-
-end_time=`date +%s`
-time3=$((end_time - start_time))
-echo '＝＝＝入力処理終了＝＝＝'
-echo '処理結果:' $?
-
-wait $!
-
-#DB→CSVチャンク
-echo '＝＝＝CSV出力処理開始＝＝＝'
-start_time=`date +%s`
-
-java -cp 'lib/*:target/*' \
-org.springframework.batch.core.launch.support.CommandLineJobRunner \
-META-INF/jobs/dbtocsv/Info2Chunk.xml \
-Info2Chunk \
-outputFile=files/output/output-info2-data.csv
-
-end_time=`date +%s`
-time4=$((end_time - start_time))
-echo '＝＝＝CSV出力処理終了＝＝＝'
-echo '処理結果:' $?
-
 echo $time1
 echo $time2
-echo $time3
-echo $time4
