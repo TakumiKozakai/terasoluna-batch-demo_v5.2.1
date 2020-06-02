5d03c2c05dd5fd2e6853b2365774806c54db40d0
T.K <2b9sa2emil@gmail.com>
2020-06-02 17:33:57 +0900

    インプットデータ変更

--- terasoluna-batch-demo/sqls/create-info1-table.sql
+++ terasoluna-batch-demo/sqls/create-info1-table.sql
@@ -1,10 +1,15 @@
 CREATE TABLE IF NOT EXISTS info1 (
-    id CHAR(8),
-    type CHAR(1),
-    status CHAR(1),
-    point INT,
+    key CHAR(20),
+    attr1 CHAR(20),
+    attr2 CHAR(12),
+    attr3 CHAR(34),
+    attr4 CHAR(100),
+    attr5 CHAR(50),
+    attr6 CHAR(50),
+    creater_name CHAR(40),
     create_date TIMESTAMP,
+    updater_name CHAR(40),
     update_date TIMESTAMP,
     delete_flg INT,
-    PRIMARY KEY(id)
+    PRIMARY KEY(key)
 );
